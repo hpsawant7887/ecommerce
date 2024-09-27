9bbf3824b4e409317dd46b7a7583e946b5e811ed
Harshad Prakash Sawant <hsawant@zscaler.com>
2024-09-26 19:53:10 -0700

    fix otel

--- src/otel_tracer.py
+++ src/otel_tracer.py
@@ -1,7 +1,8 @@
 import os
 
 from opentelemetry import trace
-from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+#from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
