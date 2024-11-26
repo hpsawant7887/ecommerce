ef90e5bc6f706b6c70a02a5708ebb163b3629713
Harshad Prakash Sawant <hsawant@zscaler.com>
2024-11-25 17:39:22 -0800

    test

--- src/ecommerce_logger.py
+++ src/ecommerce_logger.py
@@ -1,5 +1,5 @@
 import logging
-from opentelemetry.instrumentation.logging import OpenTelemetryLogEmitter
+# from opentelemetry.instrumentation.logging import OpenTelemetryLogEmitter
 
 class OptionalFieldFormatter(logging.Formatter):
     def format(self, record):
